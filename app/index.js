d3b1b0e5e81648597769dc0210370220a532eeb9
Linz Jax <linzjax@gmail.com>
2015-11-14 11:25:02 -0500

    gallery layout complete. working on adding a popover

--- app/index.js
+++ app/index.js
@@ -14,6 +14,16 @@ document.getElementById('name-link').addEventListener('click', function(e){
 	document.getElementsByClassName('bio-box')[0].classList.add('hidden');
 });
 
+var images = document.getElementsByClassName('image');
+
+var imagesArray = Array.prototype.slice.call(images);
+
+imagesArray.forEach(function(image){
+	image.addEventListener('click', function(e){
+		console.log(image.childNodes[1].getAttribute('src'));
+	});
+});
+
 //});
 
 import "styles/styles.scss";
--- public/bundle.js
+++ public/bundle.js
@@ -70,6 +70,16 @@
 		document.getElementsByClassName('bio-box')[0].classList.add('hidden');
 	});
 
+	var images = document.getElementsByClassName('image');
+
+	var imagesArray = Array.prototype.slice.call(images);
+
+	imagesArray.forEach(function (image) {
+		image.addEventListener('click', function (e) {
+			console.log(image.childNodes[1].getAttribute('src'));
+		});
+	});
+
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
